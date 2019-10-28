4a06b9247cbc537da58dbc2e6cdce28326373bc1
Lei YU <mine260309@gmail.com>
2019-10-28 17:03:20 +0800

    power-utils: Initially add Updater class
    The Updater class is used to do PSU code update, initially add it that
    does unbind/bind driver and set PSU present to false/true during the
    update.
    
    Tested: Manually verify on Witherspoon that the driver is unbind/bind,
            and the PSU present property is set to false/true during the PSU
            update:
              psutils --update \
              /xyz/openbmc_project/inventory/system/chassis/motherboard/powersupply0 \
              /tmp/images/xxxxxxxx
    
    Signed-off-by: Lei YU <mine260309@gmail.com>
    Change-Id: Ic0a9df7687303caeb9a7f21ba00dc33ee76482db

--- tools/power-utils/updater.cpp
+++ tools/power-utils/updater.cpp
@@ -17,16 +17,124 @@
 
 #include "updater.hpp"
 
+#include "utility.hpp"
+
+#include <fstream>
+#include <phosphor-logging/log.hpp>
+
+using namespace phosphor::logging;
+namespace util = phosphor::power::util;
+
 namespace updater
 {
+
+constexpr auto INVENTORY_IFACE = "xyz.openbmc_project.Inventory.Item";
+constexpr auto PRESENT_PROP = "Present";
+
+namespace internal
+{
+
+/* Get the device name from the device path */
+std::string getDeviceName(std::string devPath)
+{
+    if (devPath.back() == '/')
+    {
+        devPath.pop_back();
+    }
+    return fs::path(devPath).stem().string();
+}
+
+std::string getDevicePath(const std::string& psuInventoryPath)
+{
+    auto data = util::loadJSONFromFile(PSU_JSON_PATH);
+
+    if (data == nullptr)
+    {
+        return {};
+    }
+
+    auto devices = data.find("psuDevices");
+    if (devices == data.end())
+    {
+        log<level::WARNING>("Unable to find psuDevices");
+        return {};
+    }
+    auto devicePath = devices->find(psuInventoryPath);
+    if (devicePath == devices->end())
+    {
+        log<level::WARNING>("Unable to find path for PSU",
+                            entry("PATH=%s", psuInventoryPath.c_str()));
+        return {};
+    }
+    return *devicePath;
+}
+
+} // namespace internal
 
 std::string update(const std::string& psuInventoryPath,
                    const std::string& imageDir)
 {
+    auto devPath = internal::getDevicePath(psuInventoryPath);
+
+    // TODO: check if it is ready to update
+    // and return if not ready
+
+    Updater updater(psuInventoryPath, devPath, imageDir);
+    int ret = updater.doUpdate();
+    return ret == 0 ? "OK" : "";
+}
+
+Updater::Updater(const std::string& psuInventoryPath,
+                 const std::string& devPath, const std::string imageDir) :
+    bus(sdbusplus::bus::new_default()),
+    psuInventoryPath(psuInventoryPath), devPath(devPath),
+    devName(internal::getDeviceName(devPath)), imageDir(imageDir)
+{
+    fs::path p = fs::path(devPath) / "driver";
+    driverPath = fs::canonical(p); // Get the path that points to the driver dir
+    bindUnbind(false);
+}
+
+Updater::~Updater()
+{
+    bindUnbind(true);
+}
+
+// During PSU update, it needs to access the PSU i2c device directly, so it
+// needs to unbind the driver during the update, and re-bind after it's done.
+// After unbind, the hwmon sysfs will be gone, and the psu-monitor will report
+// errors. So set the PSU inventory's Presnet proerty to false so that
+// psu-monitor will not report any errors.
+void Updater::bindUnbind(bool doBind)
+{
+    if (!doBind)
+    {
+        // Set non-present before unbind the driver
+        setPresent(doBind);
+    }
+    auto p = driverPath;
+    p /= doBind ? "bind" : "unbind";
+    std::ofstream out(p.string());
+    out << devName;
+
+    if (doBind)
+    {
+        // Set to present after bind the driver
+        setPresent(doBind);
+    }
+}
+
+void Updater::setPresent(bool present)
+{
+    auto service = util::getService(psuInventoryPath, INVENTORY_IFACE, bus);
+    util::setProperty(INVENTORY_IFACE, PRESENT_PROP, psuInventoryPath, service,
+                      bus, present);
+}
+
+int Updater::doUpdate()
+{
     // TODO
-    fprintf(stderr, "psu: %s, image: %s\n", psuInventoryPath.c_str(),
-            imageDir.c_str());
-    return "OK";
+    return 0;
 }
 
 } // namespace updater
--- tools/power-utils/updater.hpp
+++ tools/power-utils/updater.hpp
@@ -15,10 +15,14 @@
  */
 #pragma once
 
+#include <filesystem>
+#include <sdbusplus/bus.hpp>
 #include <string>
 
 namespace updater
 {
+
+namespace fs = std::filesystem;
 
 /**
  * Get the software version of the PSU
@@ -29,5 +33,77 @@ namespace updater
  */
 std::string update(const std::string& psuInventoryPath,
                    const std::string& imageDir);
+
+class Updater
+{
+  public:
+    Updater() = delete;
+    Updater(const Updater&) = delete;
+    Updater& operator=(const Updater&) = delete;
+    Updater(Updater&&) = default;
+    Updater& operator=(Updater&&) = default;
+
+    /**
+     * @brief Constructor
+     *
+     * @param psuInventoryPath - The PSU inventory path
+     * @param devPath - The PSU device path
+     * @param imageDir - The update image directory
+     */
+    Updater(const std::string& psuInventoryPath, const std::string& devPath,
+            const std::string imageDir);
+
+    /** @brief Destructor */
+    ~Updater();
+
+    /** @brief Bind or unbind the driver
+     *
+     * @param doBind - indicate if it's going to bind or unbind the driver
+     */
+    void bindUnbind(bool doBind);
+
+    /** @brief Set the PSU inventory present property
+     *
+     * @param present - The present state to set
+     */
+    void setPresent(bool present);
+
+    /** @brief Do the PSU update
+     *
+     * @return 0 if success, otherwise non-zero
+     */
+    int doUpdate();
+
+  private:
+    /** @brief The sdbusplus DBus bus connection */
+    sdbusplus::bus::bus bus;
+
+    /** @brief The PSU inventory path */
+    std::string psuInventoryPath;
+
+    /** @brief The PSU device path
+     *
+     * Usually it is a device in i2c subsystem, e.g.
+     *   /sys/bus/i2c/devices/3-0068
+     */
+    std::string devPath;
+
+    /** @brief The PSU device name
+     *
+     * Usually it is a i2c device name, e.g.
+     *   3-0068
+     */
+    std::string devName;
+
+    /** @brief The PSU image directory */
+    std::string imageDir;
+
+    /** @brief The PSU device driver's path
+     *
+     * Usually it is the PSU driver, e.g.
+     *   /sys/bus/i2c/drivers/ibm-cffps
+     */
+    fs::path driverPath;
+};
 
 } // namespace updater
