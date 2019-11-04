c8ca42c767cb728ef2a7e782fd9d912c2c366ae6
Lei YU <mine260309@gmail.com>
2019-11-04 16:53:39 +0800

    tools: Initially add i2c tool and mock
    The power-utils will invoke i2c get/set commands to the i2c device for
    PSU code update.
    Create a separated i2c tool because it is logically standalone and could
    be shared for other utils.
    Also add an I2CInterface and its mock to make it easier to test when
    developing i2c related operations.
    
    Signed-off-by: Lei YU <mine260309@gmail.com>
    Change-Id: If5383547e6d84c0a79610e2d2d6f2fa8ee9dc061

---
+++ tools/i2c/i2c_interface.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+namespace i2c
+{
+
+class I2CInterface
+{
+  public:
+    virtual ~I2CInterface() = default;
+
+    /* @brief Read data from i2c
+     *
+     * @param[in] addr - The register address of the i2c device
+     * @param[in] size - The size of data to read
+     * @param[out] data - The data read from the i2c device
+     */
+    virtual void read(uint8_t addr, uint8_t size,
+                      std::vector<uint8_t>& data) = 0;
+
+    /* @brief Write data to i2c
+     *
+     * @param[in] addr - The register address of the i2c device
+     * @param[in] size - The size of data to read
+     * @param[in] data - The data to write to the i2c device
+     *
+     * @return true if write is successful, false otherwise
+     */
+    virtual bool write(uint8_t addr, uint8_t size, const uint8_t* data) = 0;
+};
+
+/* @brief Create an I2CInterface instance
+ *
+ * @param[in] busId - The i2c bus ID
+ * @param[in] devAddr - The device address of the i2c
+ *
+ * @return The unique_ptr holding the I2CInterface
+ */
+std::unique_ptr<I2CInterface> Create(uint8_t busId, uint8_t devAddr);
+
+} // namespace i2c
