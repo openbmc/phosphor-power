8f2ffc66ec03e75409c207b89b120b981bd5f1ac
Lei YU <mine260309@gmail.com>
2019-11-06 18:08:25 +0800

    i2c: Implement read function
    Implement I2CDevice::read() by invoking i2c_smbus_read_xxx() APIs.
    The code is referenced from i2c-tools' i2cget.c:
    
     https://github.com/ev3dev/i2c-tools/blob/ev3dev-stretch/tools/i2cget.c
    
    Tested: Verify on Witherspoon that it reads the PSU ppgrade mode status
            register (1 byte) and CRC16 register (2 bytes) correctly.
    
    Signed-off-by: Lei YU <mine260309@gmail.com>
    Change-Id: I8759b6a35229f81120acf77f08429f7f79458b8b

--- tools/i2c/i2c.cpp
+++ tools/i2c/i2c.cpp
@@ -1,8 +1,15 @@
 #include "i2c.hpp"
 
 #include <fcntl.h>
+#include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+extern "C" {
+#include <i2c/smbus.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+}
 
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/elog.hpp>
@@ -33,11 +40,46 @@ void I2CDevice::close()
 
 bool I2CDevice::read(uint8_t addr, uint8_t size, int32_t& data)
 {
-    // TODO
-    (void)addr;
-    (void)size;
-    (void)data;
-    return false;
+    int i2cSize = I2C_SMBUS_BYTE_DATA;
+    switch (size)
+    {
+        case 0:
+            return false;
+        case 1:
+            i2cSize = I2C_SMBUS_BYTE_DATA;
+            break;
+        case 2:
+            i2cSize = I2C_SMBUS_WORD_DATA;
+            break;
+        default:
+            log<level::ERR>("Invalid i2c read size", entry("ADDR=%x", addr),
+                            entry("SIZE=%u", size));
+            return false;
+    }
+
+    if (!checkFuncs(i2cSize))
+    {
+        return false;
+    }
+    if (ioctl(fd, I2C_SLAVE_FORCE, devAddr) < 0)
+    {
+        log<level::ERR>("Failed to set address", entry("ADDR=%x", addr));
+        return false;
+    }
+    if (i2cSize == I2C_SMBUS_WORD_DATA)
+    {
+        data = i2c_smbus_read_word_data(fd, addr);
+    }
+    else if (i2cSize == I2C_SMBUS_BYTE_DATA)
+    {
+        data = i2c_smbus_read_byte_data(fd, addr);
+    }
+    if (data == -1)
+    {
+        log<level::ERR>("Failed to read i2c", entry("ERR=%s", strerror(errno)));
+        return false;
+    }
+    return true;
 }
 
 bool I2CDevice::write(uint8_t addr, uint8_t size, const uint8_t* data)
@@ -60,4 +102,57 @@ std::unique_ptr<I2CInterface> create(uint8_t busId, uint8_t devAddr)
     return I2CDevice::create(busId, devAddr);
 }
 
+bool I2CDevice::checkFuncs(int size)
+{
+    unsigned long funcs;
+
+    /* Check adapter functionality */
+    if (ioctl(fd, I2C_FUNCS, &funcs) < 0)
+    {
+        log<level::ERR>("Failed to get funcs", entry("DEV=%s", busStr.c_str()),
+                        entry("ERR=%s", strerror(errno)));
+        return false;
+    }
+
+    switch (size)
+    {
+        case I2C_SMBUS_BYTE:
+            if (!(funcs & I2C_FUNC_SMBUS_READ_BYTE))
+            {
+                log<level::ERR>("Missing SMBUS_READ_BYTE");
+                return false;
+            }
+            break;
+        case I2C_SMBUS_BYTE_DATA:
+            if (!(funcs & I2C_FUNC_SMBUS_READ_BYTE_DATA))
+            {
+                log<level::ERR>("Missing SMBUS_READ_BYTE_DATA");
+                return false;
+            }
+            break;
+
+        case I2C_SMBUS_WORD_DATA:
+            if (!(funcs & I2C_FUNC_SMBUS_READ_WORD_DATA))
+            {
+                log<level::ERR>("Missing SMBUS_READ_WORD_DATA");
+                return false;
+            }
+            break;
+        case I2C_SMBUS_BLOCK_DATA:
+            if (!(funcs & I2C_FUNC_SMBUS_WRITE_BLOCK_DATA))
+            {
+                log<level::ERR>("Missing SMBUS_WRITE_BLOCK_DATA");
+                return false;
+            }
+            break;
+        case I2C_SMBUS_I2C_BLOCK_DATA:
+            if (!(funcs & I2C_FUNC_SMBUS_WRITE_I2C_BLOCK))
+            {
+                log<level::ERR>("Missing SMBUS_WRITE_I2C_BLOCK");
+                return false;
+            }
+            break;
+    }
+    return true;
+}
 } // namespace i2c
