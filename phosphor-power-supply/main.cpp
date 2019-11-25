d8b5d8fadc85e7f648d22d3435374910ee323cc8
Brandon Wyman <bjwyman@gmail.com>
2019-11-25 16:43:21 -0600

    psu: Add code to get pollInterval from JSON file
    Create a structure to hold configuration properties to pass down to the
    power supply manager class. Add in a helper function to open JSON file
    and parse out the configuration properties. Use the helper function to
    get the pollInterval and pass it down to the power supply manager
    constructor.
    
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>
    Change-Id: Ia8cf1c148a1685e2e09703c02e469a0bff034af5

--- phosphor-power-supply/main.cpp
+++ phosphor-power-supply/main.cpp
@@ -14,38 +14,76 @@
  * limitations under the License.
  */
 #include "psu_manager.hpp"
+#include "utility.hpp"
 
 #include <CLI/CLI.hpp>
+#include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
 #include <sdeventplus/event.hpp>
 
-using namespace phosphor::power::manager;
+using namespace phosphor::power;
+
+struct json_properties
+{
+    int pollInterval;
+};
+
+void getJSONProperties(const std::string& path, json_properties& p)
+{
+    using namespace phosphor::logging;
+
+    nlohmann::json configFileJSON = util::loadJSONFromFile(path.c_str());
+
+    if (configFileJSON == nullptr)
+    {
+        throw std::runtime_error("Failed to load JSON configuration file");
+    }
+
+    p.pollInterval = configFileJSON.at("pollInterval");
+}
 
 int main(int argc, char* argv[])
 {
-    CLI::App app{"OpenBMC Power Supply Unit Monitor"};
-
-    std::string configfile;
-    app.add_option("-c,--config", configfile, "JSON configuration file path")
-        ->check(CLI::ExistingFile);
-
-    // Read the arguments.
-    CLI11_PARSE(app, argc, argv);
-    if (configfile.empty())
+    try
     {
-        configfile = "/etc/phosphor-psu-monitor/psu_config.json";
+        CLI::App app{"OpenBMC Power Supply Unit Monitor"};
+
+        std::string configfile;
+        app.add_option("-c,--config", configfile,
+                       "JSON configuration file path")
+            ->check(CLI::ExistingFile);
+
+        // Read the arguments.
+        CLI11_PARSE(app, argc, argv);
+        if (configfile.empty())
+        {
+            configfile = "/etc/phosphor-psu-monitor/psu_config.json";
+        }
+
+        // Parse out the JSON properties needed to pass down to the PSU manager.
+        json_properties properties = {0};
+        getJSONProperties(configfile, properties);
+
+        auto bus = sdbusplus::bus::new_default();
+        auto event = sdeventplus::Event::get_default();
+
+        // Attach the event object to the bus object so we can
+        // handle both sd_events (for the timers) and dbus signals.
+        bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+
+        auto pollInterval = std::chrono::milliseconds(properties.pollInterval);
+        manager::PSUManager manager(bus, event, pollInterval);
+
+        return manager.run();
     }
-
-    auto bus = sdbusplus::bus::new_default();
-    auto event = sdeventplus::Event::get_default();
-
-    // Attach the event object to the bus object so we can
-    // handle both sd_events (for the timers) and dbus signals.
-    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
-
-    // TODO: Should get polling interval from JSON file.
-    auto pollInterval = std::chrono::milliseconds(1000);
-    PSUManager manager(bus, event, pollInterval);
-
-    return manager.run();
+    catch (const std::exception& e)
+    {
+        log<level::ERR>(e.what());
+        return -1;
+    }
+    catch (...)
+    {
+        log<level::ERR>("Caught unexpected exception type");
+        return -2;
+    }
 }
