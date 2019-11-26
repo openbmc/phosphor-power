123968e4aba986da7d3768688cb4bcd5d1b1b610
Brandon Wyman <bjwyman@gmail.com>
2019-11-25 18:10:52 -0600

    psu: Updates for power supply presence
    Update the JSON file to indicate the minimum and maximum number of power
    supplies this system can have.
    
    Update the JSON file to have an array of power supply objects, each with
    their various own properties. Each power supply will have:
    1) An associated D-Bus path for the inventory. This will have the
    Present property.
    2) An associated I2C bus number.
    3) An associated I2C bus adddress.
    
    Update PowerSupply class to have a present member variable, an
    isPresent() accessor function, and two functions for updating the
    presence, one being the call back function for inventory change events,
    which will use an associated match member variable.
    
    If the inventory Present property is not available when we startup, add
    in a call to update the presence status when it is added.
    
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>
    Change-Id: Ib34510a6da66ba1b8f1e927093b3d10b09beb410

--- phosphor-power-supply/main.cpp
+++ phosphor-power-supply/main.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "power_supply.hpp"
 #include "psu_manager.hpp"
 #include "utility.hpp"
 
