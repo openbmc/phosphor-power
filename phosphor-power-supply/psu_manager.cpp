3cd1f087fabf099c0db5c727faad4bbb60d9a468
Brandon Wyman <bjwyman@gmail.com>
2019-11-25 18:10:52 -0600

    psu: Updates for power supply presence
    Update the JSON file to indicate the minimum and maximum number of power
    supplies this system can have.
    
    Update the JSON file to have an array of power supply objects, each with
    their various own properties. Each power supply will have:
    1) An associated D-Bus path for the inventory. This will have the
    Present property.
    2) An associated I2C bus number.
    3) An associated I2C bus adddress.
    
    Update PowerSupply class to have a present member variable, an
    isPresent() accessor function, and two functions for updating the
    presence, one being the call back function for inventory change events,
    which will use an associated match member variable.
    
    If the inventory Present property is not available when we startup, add
    in a call to update the presence status when it is added.
    
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>
    Change-Id: Ib34510a6da66ba1b8f1e927093b3d10b09beb410

--- phosphor-power-supply/psu_manager.cpp
+++ phosphor-power-supply/psu_manager.cpp
@@ -9,6 +9,102 @@ namespace power
 namespace manager
 {
 
+PSUManager::PSUManager(sdbusplus::bus::bus& bus, const sdeventplus::Event& e,
+                       const std::string& configfile) :
+    bus(bus)
+{
+    // Parse out the JSON properties needed to pass down to the PSU manager.
+    sys_properties properties;
+    std::vector<std::unique_ptr<PowerSupply>> lpsus;
+    getJSONProperties(configfile, bus, properties, lpsus);
+
+    using namespace sdeventplus;
+    auto interval = std::chrono::milliseconds(properties.pollInterval);
+    timer = std::make_unique<utility::Timer<ClockId::Monotonic>>(
+        e, std::bind(&PSUManager::analyze, this), interval);
+
+    minPSUs = {properties.minPowerSupplies};
+    maxPSUs = {properties.maxPowerSupplies};
+
+    psus = {std::move(lpsus)};
+
+    // Subscribe to power state changes
+    powerService = util::getService(POWER_OBJ_PATH, POWER_IFACE, bus);
+    powerOnMatch = std::make_unique<sdbusplus::bus::match_t>(
+        bus,
+        sdbusplus::bus::match::rules::propertiesChanged(POWER_OBJ_PATH,
+                                                        POWER_IFACE),
+        [this](auto& msg) { this->powerStateChanged(msg); });
+
+    initialize();
+}
+
+void PSUManager::getJSONProperties(
+    const std::string& path, sdbusplus::bus::bus& bus, sys_properties& p,
+    std::vector<std::unique_ptr<PowerSupply>>& psus)
+{
+    using namespace phosphor::logging;
+
+    nlohmann::json configFileJSON = util::loadJSONFromFile(path.c_str());
+
+    if (configFileJSON == nullptr)
+    {
+        throw std::runtime_error("Failed to load JSON configuration file");
+    }
+
+    if (!configFileJSON.contains("SystemProperties"))
+    {
+        throw std::runtime_error("Missing required SystemProperties");
+    }
+
+    if (!configFileJSON.contains("PowerSupplies"))
+    {
+        throw std::runtime_error("Missing required PowerSupplies");
+    }
+
+    auto SysProps = configFileJSON["SystemProperties"];
+
+    if (!SysProps.contains("pollInterval"))
+    {
+        throw std::runtime_error("Missing required pollInterval property");
+    }
+
+    p.pollInterval = SysProps["pollInterval"];
+
+    if (SysProps.contains("MinPowerSupplies"))
+    {
+        p.minPowerSupplies = SysProps["MinPowerSupplies"];
+    }
+    else
+    {
+        p.minPowerSupplies = 0;
+    }
+
+    if (SysProps.contains("MaxPowerSupplies"))
+    {
+        p.maxPowerSupplies = SysProps["MaxPowerSupplies"];
+    }
+    else
+    {
+        p.maxPowerSupplies = 0;
+    }
+
+    for (auto psuJSON : configFileJSON["PowerSupplies"])
+    {
+        if (psuJSON.contains("Inventory"))
+        {
+            std::string invpath = psuJSON["Inventory"];
+            auto psu = std::make_unique<PowerSupply>(bus, invpath);
+            psus.emplace_back(std::move(psu));
+        }
+    }
+
+    if (psus.empty())
+    {
+        throw std::runtime_error("No power supplies to monitor");
+    }
+}
+
 void PSUManager::powerStateChanged(sdbusplus::message::message& msg)
 {
     int32_t state = 0;
