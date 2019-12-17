733a91021017ce471c3e6d4085e0766b4f496e90
Bob King <Bob_King@wistron.com>
2019-12-17 18:11:57 +0800

    Add validate-regulators-config.py validation tool.
    Validate-regulators-config.py is a tool validates configuration
    files for the phosphor-regulators application. See
    phosphor-regulators/docs/config_file/README.mdREADME.md for the
    command to use the tool.
    
    Signed-off-by: Bob King <Bob_King@wistron.com>
    Change-Id: I41cd4070dbb6ec27e834308a9eb0a819feb77319

---
+++ phosphor-regulators/tools/validate-regulators-config.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import sys
+import jsonschema
+
+r"""
+Validates the phosphor-regulators configuration file. Checks it against a JSON
+schema as well as doing some extra checks that can't be encoded in the schema.
+"""
+
+def validate_schema(config, schema):
+    r"""
+    Validates the specified config file using the specified
+    schema file.
+
+    config:   Path of the file containing the config JSON
+    schema:   Path of the file containing the schema JSON
+    """
+
+    with open(config) as config_handle:
+        config_json = json.load(config_handle)
+
+        with open(schema) as schema_handle:
+            schema_json = json.load(schema_handle)
+
+            try:
+                jsonschema.validate(config_json, schema_json)
+            except jsonschema.ValidationError as e:
+                print(e)
+                sys.exit("Validation failed.")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='phosphor-regulators configuration file validator')
+
+    parser.add_argument('-s', '--schema-file', dest='schema_file',
+                        help='The phosphor-regulators schema file')
+
+    parser.add_argument('-c', '--configuration-file', dest='configuration_file',
+                        help='The phosphor-regulators configuration file')
+
+    args = parser.parse_args()
+
+    if not args.schema_file:
+        parser.print_help()
+        sys.exit("Error: Schema file is required.")
+    if not args.configuration_file:
+        parser.print_help()
+        sys.exit("Error: Configuration file is required.")
+
+    validate_schema(args.configuration_file, args.schema_file)
