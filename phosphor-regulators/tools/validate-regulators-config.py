dd5c634720507e83d8af9bfda43c1a717f6c24bf
Bob King <Bob_King@wistron.com>
2020-01-15 14:45:06 +0800

    Add check duplicate ID function.
    Add check duplicate id and number functions.
    check_duplicate_rule_id
    check_duplicate_chassis_number
    check_duplicate_device_id
    check_duplicate_rail_id
    
    Signed-off-by: Bob King <Bob_King@wistron.com>
    Change-Id: Ic7874037ee46614ca34f88113adc4d246608b718

--- phosphor-regulators/tools/validate-regulators-config.py
+++ phosphor-regulators/tools/validate-regulators-config.py
@@ -10,6 +10,73 @@ Validates the phosphor-regulators configuration file. Checks it against a JSON
 schema as well as doing some extra checks that can't be encoded in the schema.
 """
 
+def check_duplicate_rule_id(config_json):
+    r"""
+    Check that there aren't any "rule" entries with the same 'id' field.
+    config_json: The patform config JSON
+    """
+    ruleID = {}
+    if 'rules' in config_json:
+        for entry in config_json['rules']:
+            if entry['id'] in ruleID.keys():
+                print("Found multiple rule ID of error {}".format(entry['id']))
+                sys.exit("Duplicate ID failed.")
+            else:
+                ruleID[entry['id']] = {}
+
+def check_duplicate_chassis_number(config_json):
+    r"""
+    Check that there aren't any "chassis" entries with the same'device' field.
+    config_json: The patform config JSON
+    """
+    number = {}
+    if 'chassis' in config_json:
+        for entry in config_json['chassis']:
+            if entry['number'] in number.keys():
+                print("Found multiple chassis number of error {}"\
+                .format(entry['number']))
+                sys.exit("Duplicate number failed.")
+            else:
+                number[entry['number']] = {}
+
+def check_duplicate_device_id(config_json):
+    r"""
+    Check that there aren't any "chassis/devices" entries with the same 'id'
+    field.
+    config_json: The patform config JSON
+    """
+    deviceID = {}
+    if 'chassis' in config_json:
+        for entry in config_json['chassis']:
+            if 'devices' in entry:
+                for entry in entry['devices']:
+                    if entry['id'] in deviceID.keys():
+                        print("Found multiple devices ID of error {}"\
+                        .format(entry['id']))
+                        sys.exit("Duplicate ID failed.")
+                    else:
+                        deviceID[entry['id']] = {}
+
+def check_duplicate_rail_id(config_json):
+    r"""
+    Check that there aren't any "chassis/devices/rails" entries with the same
+    'id' field.
+    config_json: The patform config JSON
+    """
+    railID = {}
+    if 'chassis' in config_json:
+        for entry in config_json['chassis']:
+            if 'devices' in entry:
+                for entry in entry['devices']:
+                    if 'rails' in entry:
+                        for entry in entry['rails']:
+                            if entry['id'] in railID.keys():
+                                print("Found multiple rails ID of error {}"\
+                                .format(entry['id']))
+                                sys.exit("Duplicate ID failed.")
+                            else:
+                                railID[entry['id']] = {}
+
 def validate_schema(config, schema):
     r"""
     Validates the specified config file using the specified
@@ -30,6 +97,14 @@ def validate_schema(config, schema):
             except jsonschema.ValidationError as e:
                 print(e)
                 sys.exit("Validation failed.")
+
+        check_duplicate_rule_id(config_json)
+
+        check_duplicate_chassis_number(config_json)
+
+        check_duplicate_device_id(config_json)
+
+        check_duplicate_rail_id(config_json)
 
 if __name__ == '__main__':
 
