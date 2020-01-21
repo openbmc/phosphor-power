9620e08291db519de9725cc719f1a3c596d6af94
Bob King <Bob_King@wistron.com>
2020-01-21 11:08:50 +0800

    Add test cases for object 'compare_presence'.
    Test cases for object 'compare_presence' are:
    Valid.
    Invalid: no FRU property.
    Invalid: FRU property value is string less than 1.
    Invalid: no vlaue property.
    Invalid: value property type is not boolean.
    Invalid: FRU property type is not string.
    
    Signed-off-by: Bob King <Bob_King@wistron.com>
    Change-Id: I051d87cadeb79a6612df5a483c2149569613d518

--- phosphor-regulators/test/validate-regulators-config_tests.cpp
+++ phosphor-regulators/test/validate-regulators-config_tests.cpp
@@ -348,3 +348,56 @@ TEST(ValidateRegulatorsConfigTest, And)
                             "u'foo' is not of type u'object'");
     }
 }
+TEST(ValidateRegulatorsConfigTest, RuleComparePresence)
+{
+    json comparePresenceFile = validConfigFile;
+    comparePresenceFile["rules"][0]["actions"][1]["compare_presence"]["fru"] =
+        "/system/chassis/motherboard/regulator2";
+    comparePresenceFile["rules"][0]["actions"][1]["compare_presence"]["value"] =
+        true;
+    // test rule actions compare_presence valid.
+    {
+        json configFile = comparePresenceFile;
+        EXPECT_JSON_VALID(configFile);
+    }
+
+    // test rule actions compare_presence with no fru invalid.
+    {
+        json configFile = comparePresenceFile;
+        configFile["rules"][0]["actions"][1]["compare_presence"].erase("fru");
+        EXPECT_JSON_INVALID(configFile, "Validation failed.",
+                            "u'fru' is a required property");
+    }
+
+    // test rule actions compare_presence with FRU string less than 1 invalid.
+    {
+        json configFile = comparePresenceFile;
+        configFile["rules"][0]["actions"][1]["compare_presence"]["fru"] = "";
+        EXPECT_JSON_INVALID(configFile, "Validation failed.",
+                            "u'' is too short");
+    }
+
+    // test rule actions compare_presence with no value invalid.
+    {
+        json configFile = comparePresenceFile;
+        configFile["rules"][0]["actions"][1]["compare_presence"].erase("value");
+        EXPECT_JSON_INVALID(configFile, "Validation failed.",
+                            "u'value' is a required property");
+    }
+
+    // test rule actions compare_presence with value data not boolean invalid.
+    {
+        json configFile = comparePresenceFile;
+        configFile["rules"][0]["actions"][1]["compare_presence"]["value"] = "1";
+        EXPECT_JSON_INVALID(configFile, "Validation failed.",
+                            "u'1' is not of type u'boolean'");
+    }
+
+    // test rule actions compare_presence with FRU not string invalid.
+    {
+        json configFile = comparePresenceFile;
+        configFile["rules"][0]["actions"][1]["compare_presence"]["fru"] = 1;
+        EXPECT_JSON_INVALID(configFile, "Validation failed.",
+                            "1 is not of type u'string'");
+    }
+}
