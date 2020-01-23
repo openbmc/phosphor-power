3a6957c4ed9f68618addd8b058af49f49ef925ad
Matthew Barth <msbarth@us.ibm.com>
2020-01-23 13:40:49 -0600

    regs: Add manager interface object
    Create a manager object that extends the manager interface overriding
    the configure and monitor methods. This provides the framework for
    receiving dbus method calls at the appropriate times during a system
    poweron or poweroff.
    
    Tested:
        Used `busctl` to call the configure method
        Used `busctl` to call the monitor method
    
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
    Change-Id: I09db5a800a09205ba182824e62bacb19b10856bd

---
+++ phosphor-regulators/src/manager.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <interfaces/manager.hpp>
+
+namespace phosphor
+{
+namespace power
+{
+namespace regulators
+{
+
+constexpr auto busName = "xyz.openbmc_project.Power.Regulators";
+constexpr auto objPath = "/xyz/openbmc_project/power/regulators/manager";
+
+using ManagerObject = sdbusplus::server::object::object<
+    phosphor::power::regulators::server::Manager>;
+
+class Manager : public ManagerObject
+{
+  public:
+    Manager() = delete;
+    Manager(const Manager&) = delete;
+    Manager(Manager&&) = delete;
+    Manager& operator=(const Manager&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    ~Manager() = default;
+
+    /**
+     * Constructor
+     * Creates a manager of the handling regulators.
+     *
+     * @param[in] bus - the dbus bus
+     */
+    Manager(sdbusplus::bus::bus& bus);
+
+    /**
+     * @brief Overridden manager object's configure method
+     */
+    void configure() override;
+
+    /**
+     * @brief Overridden manager object's monitor method
+     *
+     * @param[in] enable - Enable or disable regulator monitoring
+     */
+    void monitor(bool enable) override;
+
+  private:
+    /**
+     * The dbus bus
+     */
+    sdbusplus::bus::bus& bus;
+};
+
+} // namespace regulators
+} // namespace power
+} // namespace phosphor
