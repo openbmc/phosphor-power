2554a278b18b708b70be56263da6f136a05dba29
Matthew Barth <msbarth@us.ibm.com>
2020-01-23 13:40:49 -0600

    WIP: Add manager interface object
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
    Change-Id: I09db5a800a09205ba182824e62bacb19b10856bd

---
+++ phosphor-regulators/src/manager.cpp
@@ -0,0 +1,68 @@
+/**
+ * Copyright © 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "manager.hpp"
+
+#include <sdbusplus/bus.hpp>
+
+#include <chrono>
+#include <thread>
+
+namespace phosphor
+{
+namespace power
+{
+namespace regulators
+{
+
+Manager::Manager(sdbusplus::bus::bus& bus) :
+    ManagerObject(bus, objPath, true), bus(bus)
+{
+    // TODO get property (IM keyword)
+    // call parse json function
+    // TODO subscribe to interfacesadded (IM keyword)
+    // callback would call parse json function
+    bus.request_name(busName);
+}
+
+void Manager::configure()
+{
+    printf(">>> config()\n");
+    for (int i = 5; i > 0; --i)
+    {
+        printf("*** %i ***\n", i);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    printf("<<< config()\n");
+}
+
+void Manager::monitor(bool enable)
+{
+    printf(">>> monitor()\n");
+    if (enable)
+    {
+        for (int i = 5; i > 0; --i)
+        {
+            printf("*** %i ***\n", i);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+    printf("<<< monitor()\n");
+}
+
+} // namespace regulators
+} // namespace power
+} // namespace phosphor
---
+++ phosphor-regulators/src/manager.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Power/Regulators/Manager/server.hpp>
+
+namespace phosphor
+{
+namespace power
+{
+namespace regulators
+{
+
+constexpr auto busName = "xyz.openbmc_project.Power.Regulators";
+constexpr auto objPath = "/xyz/openbmc_project/power/regulators/manager";
+
+using ManagerObject = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Power::Regulators::server::Manager>;
+
+class Manager : public ManagerObject
+{
+  public:
+    Manager() = delete;
+    Manager(const Manager&) = delete;
+    Manager(Manager&&) = delete;
+    Manager& operator=(const Manager&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    ~Manager() = default;
+
+    Manager(sdbusplus::bus::bus& bus);
+
+    virtual void configure();
+
+    virtual void monitor(bool enable);
+
+  private:
+    sdbusplus::bus::bus& bus;
+};
+
+} // namespace regulators
+} // namespace power
+} // namespace phosphor
