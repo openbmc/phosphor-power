7c513a96b9ad90d44282d78b91d8545db1f34d8b
Brandon Wyman <bjwyman@gmail.com>
2020-01-28 13:11:25 -0600

    Update analyze function to check STATUS_WORD
    The STATUS_WORD PMBus command response will be the start of the power
    supply fault analysis. Update the analyze() function to read its value
    and process (select) fault bits.
    
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>
    Change-Id: If7274ad237c0604a56008676ae64804a5fd2854e

---
+++ phosphor-power-supply/test/mock.cpp
@@ -0,0 +1,15 @@
+#include <mock.hpp>
+
+namespace phosphor
+{
+namespace pmbus
+{
+
+std::unique_ptr<PMBusBase> createPMBus(std::uint8_t /*bus*/,
+                                       const std::string& /*address*/)
+{
+    return std::make_unique<MockedPMBus>();
+}
+
+} // namespace pmbus
+} // namespace phosphor
