a2ce77802e46b7a2a1bdffcb3e91844234de2e9c
Matthew Barth <msbarth@us.ibm.com>
2020-01-29 14:42:47 -0600

    regs: Add an event timer for monitoring
    Create and enable a repeating 1 second timer when monitoring is enabled.
    Delete the timer when monitoring is disabled.
    
    Tested:
        Timer created when monitoring is enabled
        Each timer expiration calls bound callback function
        Timer stopped/deleted when monitoring is disabled
    
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
    Change-Id: I990124fb727bb0a0545ce06f9c878e81b572c7a5

--- phosphor-regulators/src/manager.hpp
+++ phosphor-regulators/src/manager.hpp
@@ -2,6 +2,8 @@
 
 #include <sdbusplus/bus.hpp>
 #include <interfaces/manager.hpp>
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/utility/timer.hpp>
 
 namespace phosphor
 {
@@ -12,6 +14,8 @@ namespace regulators
 
 constexpr auto busName = "xyz.openbmc_project.Power.Regulators";
 constexpr auto objPath = "/xyz/openbmc_project/power/regulators/manager";
+
+using Timer = sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic>;
 
 using ManagerObject = sdbusplus::server::object::object<
     phosphor::power::regulators::server::Manager>;
@@ -31,8 +35,9 @@ class Manager : public ManagerObject
      * Creates a manager of the handling regulators.
      *
      * @param[in] bus - the dbus bus
+     * @param[in] event - the sdevent event
      */
-    Manager(sdbusplus::bus::bus& bus);
+    Manager(sdbusplus::bus::bus& bus, const sdeventplus::Event& event);
 
     /**
      * @brief Overridden manager object's configure method
@@ -46,11 +51,26 @@ class Manager : public ManagerObject
      */
     void monitor(bool enable) override;
 
+    /**
+     * @brief Timer expired callback function
+     */
+    void timerExpired();
+
   private:
     /**
      * The dbus bus
      */
     sdbusplus::bus::bus& bus;
+
+    /**
+     * Event to loop on
+     */
+    sdeventplus::Event eventLoop;
+
+    /**
+     * List of event timers
+     */
+    std::vector<Timer> timers;
 };
 
 } // namespace regulators
