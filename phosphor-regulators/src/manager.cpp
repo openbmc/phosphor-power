5223147f8fd7222daee9533aeb4caab334dfcc96
Matthew Barth <msbarth@us.ibm.com>
2020-01-29 14:42:47 -0600

    regs: Add an event timer for monitoring
    Create and enable a repeating 1 second timer when monitoring is enabled.
    Delete the timer when monitoring is disabled.
    
    Tested:
        Timer created when monitoring is enabled
        Each timer expiration calls bound callback function
        Timer stopped/deleted when monitoring is disabled
    
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
    Change-Id: I990124fb727bb0a0545ce06f9c878e81b572c7a5

--- phosphor-regulators/src/manager.cpp
+++ phosphor-regulators/src/manager.cpp
@@ -17,6 +17,7 @@
 #include "manager.hpp"
 
 #include <sdbusplus/bus.hpp>
+#include <chrono>
 
 namespace phosphor
 {
@@ -25,8 +26,8 @@ namespace power
 namespace regulators
 {
 
-Manager::Manager(sdbusplus::bus::bus& bus) :
-    ManagerObject(bus, objPath, true), bus(bus)
+Manager::Manager(sdbusplus::bus::bus& bus, const sdeventplus::Event& event) :
+    ManagerObject(bus, objPath, true), bus(bus), eventLoop(event)
 {
     // TODO get property (IM keyword)
     // call parse json function
@@ -48,12 +49,22 @@ void Manager::monitor(bool enable)
 {
     if (enable)
     {
-        // TODO Enable timer event that will do the monitoring
+        Timer timer(eventLoop, std::bind(&Manager::timerExpired, this));
+        // Set timer as a repeating 1sec timer
+        timer.restart(std::chrono::milliseconds(1000));
+        timers.emplace_back(std::move(timer));
     }
     else
     {
-        // TODO Disable/delete timer event to stop monitoring
+        // Delete all timers to disable monitoring
+        timers.clear();
     }
+}
+
+void Manager::timerExpired()
+{
+    // TODO Analyze, refresh sensor status, and
+    // collect/update telemetry for each regulator
 }
 
 } // namespace regulators
