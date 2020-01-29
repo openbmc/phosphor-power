aa35da67402932c78c526ba99707369763d9c691
Matthew Barth <msbarth@us.ibm.com>
2020-01-29 15:13:13 -0600

    regs: Handle HUP signals
    Enable capturing and handling a HUP signal to reload configuration data.
    
    Tested:
        Handler function called upon receiving a SIGHUP
    
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
    Change-Id: I41d136ede5f2a4c0bf595ee58993a4408f153013

--- phosphor-regulators/src/manager.cpp
+++ phosphor-regulators/src/manager.cpp
@@ -67,6 +67,18 @@ void Manager::timerExpired()
     // collect/update telemetry for each regulator
 }
 
+void Manager::sighupHandler(sdeventplus::source::Signal& sigSrc,
+                            const struct signalfd_siginfo* sigInfo)
+{
+    // Suppress "unused parameter" errors
+    // *Note: These may or may not be needed when
+    // reloading the configuration data
+    (void)sigSrc;
+    (void)sigInfo;
+
+    // TODO Reload and process the configuration data
+}
+
 } // namespace regulators
 } // namespace power
 } // namespace phosphor
