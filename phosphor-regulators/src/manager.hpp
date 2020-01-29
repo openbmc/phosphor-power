f740ef82b75aa59f8b8b77b85432e57ecd8d78ff
Matthew Barth <msbarth@us.ibm.com>
2020-01-29 15:13:13 -0600

    regs: Handle HUP signals
    Enable capturing and handling a HUP signal to reload configuration data.
    
    Tested:
        Handler function called upon receiving a SIGHUP
    
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
    Change-Id: I41d136ede5f2a4c0bf595ee58993a4408f153013

--- phosphor-regulators/src/manager.hpp
+++ phosphor-regulators/src/manager.hpp
@@ -3,6 +3,7 @@
 #include <interfaces/manager.hpp>
 #include <sdbusplus/bus.hpp>
 #include <sdeventplus/event.hpp>
+#include <sdeventplus/source/signal.hpp>
 #include <sdeventplus/utility/timer.hpp>
 
 #include <sdbusplus/server/object.hpp>
@@ -57,6 +58,16 @@ class Manager : public ManagerObject
      * @brief Timer expired callback function
      */
     void timerExpired();
+
+    /**
+     * @brief Callback function to handle receiving a HUP signal
+     * to reload the configuration data.
+     *
+     * @param[in] sigSrc - sd_event_source signal wrapper
+     * @param[in] sigInfo - signal info on signal fd
+     */
+    void sighupHandler(sdeventplus::source::Signal& sigSrc,
+                       const struct signalfd_siginfo* sigInfo);
 
   private:
     /**
