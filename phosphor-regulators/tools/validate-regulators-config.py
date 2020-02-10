ae944ec153e954ac1872aa94bb88aafbfe0a0ff3
Bob King <Bob_King@wistron.com>
2020-02-10 15:40:53 +0800

    regulators: Add check set device value exist function.
    Add check_set_device_value_exist function.
    It check set device value mapping to an exist device rule in the config file.
    
    Signed-off-by: Bob King <Bob_King@wistron.com>
    Change-Id: I95e6f9b51cb2c75c3d95b3ca1c4377f4b6e0171c

--- phosphor-regulators/tools/validate-regulators-config.py
+++ phosphor-regulators/tools/validate-regulators-config.py
@@ -13,6 +13,25 @@ schema as well as doing some extra checks that can't be encoded in the schema.
 def handle_validation_error():
     sys.exit("Validation failed.")
 
+def check_set_device_value_exist(config_json):
+    r"""
+    Check if a set_device action specifies a device ID that does not exist.
+    config_json: Configuration file JSON
+    """
+    ids = []
+    for chassis in config_json.get('chassis', {}):
+        for device in chassis.get('devices', {}):
+            ids.append(device['id'])
+    for rule in config_json.get('rules', {}):
+        for action in rule.get('actions', {}):
+            if 'set_device' in action:
+                set_device_value = action['set_device']
+                if set_device_value not in ids:
+                    sys.stderr.write("Error: Device ID does not exist.\n"+\
+                    "Found set_device action that specifies invalid device ID "\
+                    +set_device_value+'\n')
+                    handle_validation_error()
+
 def check_run_rule_value_exist(config_json):
     r"""
     Check if any run_rule actions specify a rule ID that does not exist.
@@ -216,3 +235,5 @@ if __name__ == '__main__':
     check_infinite_loops(config_json)
 
     check_run_rule_value_exist(config_json)
+
+    check_set_device_value_exist(config_json)
