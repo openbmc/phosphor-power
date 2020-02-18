5fb87b2ea8d501a73afa695bf0259fabc465af56
Bob King <Bob_King@wistron.com>
2020-02-18 13:36:18 +0800

    regulators: Add test cases for check_number_in_masks
    *Invalid: test number of elements in masks not equal to number in values.
    
    Signed-off-by: Bob King <Bob_King@wistron.com>
    Change-Id: I523782a46c2b52035bfa2b6b4531ce79c37a62f9

--- phosphor-regulators/test/validate-regulators-config_tests.cpp
+++ phosphor-regulators/test/validate-regulators-config_tests.cpp
@@ -2476,3 +2476,18 @@ TEST(ValidateRegulatorsConfigTest, RuleIDExist)
         EXPECT_JSON_INVALID(configFile, "Error: Rule ID does not exist.", "");
     }
 }
+TEST(ValidateRegulatorsConfigTest, NumberOfElementsInMasks)
+{
+    // Invalid: test number of elements in masks not equal to number in values.
+    {
+        json configFile = validConfigFile;
+        configFile["rules"][0]["actions"][1]["i2c_compare_bytes"]["register"] =
+            "0x82";
+        configFile["rules"][0]["actions"][1]["i2c_compare_bytes"]["values"] = {
+            "0x02", "0x73"};
+        configFile["rules"][0]["actions"][1]["i2c_compare_bytes"]["masks"] = {
+            "0x7F"};
+        EXPECT_JSON_INVALID(configFile, "Error: Masks elements number error.",
+                            "");
+    }
+}
