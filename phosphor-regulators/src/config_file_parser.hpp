6ff7666fa350e755ab8291d73b211164e957411e
Bob King <Bob_King@wistron.com>
2020-04-06 11:34:09 +0800

    regulators: Implements support for device and i2c_interface
    Implements support for parsing the device and i2c_interface JSON elements in the
    configuration file parser.
    
    Signed-off-by: Bob King <Bob_King@wistron.com>
    Change-Id: I8cf55d4fc793357113cdfcb6c543556b5eb09a8d

--- phosphor-regulators/src/config_file_parser.hpp
+++ phosphor-regulators/src/config_file_parser.hpp
@@ -17,11 +17,15 @@
 
 #include "action.hpp"
 #include "chassis.hpp"
+#include "configuration.hpp"
 #include "device.hpp"
+#include "i2c_interface.hpp"
 #include "i2c_write_bit_action.hpp"
 #include "i2c_write_byte_action.hpp"
 #include "i2c_write_bytes_action.hpp"
 #include "pmbus_write_vout_command_action.hpp"
+#include "presence_detection.hpp"
+#include "rail.hpp"
 #include "rule.hpp"
 #include "run_rule_action.hpp"
 
@@ -198,6 +202,18 @@ std::vector<std::unique_ptr<Chassis>>
     parseChassisArray(const nlohmann::json& element);
 
 /**
+ * Parses a JSON element containing a device.
+ *
+ * Returns the corresponding C++ Device object.
+ *
+ * Throws an exception if parsing fails.
+ *
+ * @param element JSON element
+ * @return Device object
+ */
+std::unique_ptr<Device> parseDevice(const nlohmann::json& element);
+
+/**
  * Parses a JSON element containing an array of devices.
  *
  * Returns the corresponding C++ Device objects.
@@ -278,6 +294,19 @@ inline uint8_t parseHexByte(const nlohmann::json& element)
 std::vector<uint8_t> parseHexByteArray(const nlohmann::json& element);
 
 /**
+ * Parses a JSON element containing an i2c_interface action.
+ *
+ * Returns the corresponding C++ i2c::I2CInterface object.
+ *
+ * Throws an exception if parsing fails.
+ *
+ * @param element JSON element
+ * @return i2c::I2CInterface object
+ */
+std::unique_ptr<i2c::I2CInterface>
+    parseI2CInterface(const nlohmann::json& element);
+
+/**
  * Parses a JSON element containing an i2c_write_bit action.
  *
  * Returns the corresponding C++ I2CWriteBitAction object.
@@ -353,6 +382,19 @@ inline int8_t parseInt8(const nlohmann::json& element)
  */
 std::unique_ptr<PMBusWriteVoutCommandAction>
     parsePMBusWriteVoutCommand(const nlohmann::json& element);
+
+/**
+ * Parses a JSON element containing an array of rails.
+ *
+ * Returns the corresponding C++ Rail objects.
+ *
+ * Throws an exception if parsing fails.
+ *
+ * @param element JSON element
+ * @return vector of Rail objects
+ */
+std::vector<std::unique_ptr<Rail>>
+    parseRailArray(const nlohmann::json& element);
 
 /**
  * Parses the JSON root element of the entire configuration file.
