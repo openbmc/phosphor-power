392a99cd8b3d17fe8c5eb9499ae854cb27a5fd68
Matthew Barth <msbarth@us.ibm.com>
2020-04-13 13:52:49 -0500

    regs: Create regulator control utility function
    The regulator control program will call methods on the
    phosphor-regulators service to control the mode in which the
    phosphor-regulators application should run in.
    
    Tested:
        Dbus method call occurs against the phosphor-regulators service
    
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
    Change-Id: Ib5829f271883bee2ebd52d33cb75da603752e0ae

---
+++ phosphor-regulators/src/regsctl/utility.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <phosphor-logging/log.hpp>
+#include <sdbusplus/bus.hpp>
+
+namespace phosphor::power::regulators::control
+{
+
+constexpr auto busName = "xyz.openbmc_project.Power.Regulators";
+constexpr auto objPath = "/xyz/openbmc_project/power/regulators/manager";
+constexpr auto interface = "xyz.openbmc_project.Power.Regulators.Manager";
+
+using namespace phosphor::logging;
+
+/**
+ * @brief Call a dbus method
+ * @param[in] method - Method name to call
+ * @param[in] args - Any needed arguments to the method
+ *
+ * @return Response message from the method call
+ */
+template <typename ...Args>
+auto callMethod(
+    const std::string& method,
+    Args&& ... args)
+{
+    auto bus = sdbusplus::bus::new_default();
+    auto reqMsg = bus.new_method_call(busName, objPath, interface,
+        method.c_str());
+    reqMsg.append(std::forward<Args>(args)...);
+
+    sdbusplus::message::message respMsg;
+    try
+    {
+        respMsg = bus.call(reqMsg);
+    }
+    catch (const sdbusplus::exception::SdBusError&)
+    {
+        log<level::ERR>("Error on regulator control method call",
+            entry("METHOD=%s", method.c_str()));
+    }
+    return respMsg;
+}
+
+}// namespace phosphor::power::regulators::control
