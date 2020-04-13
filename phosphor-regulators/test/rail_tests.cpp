fd6db397c357f41afecf058b9d530de38f1af59d
Shawn McCarney <shawnmm@us.ibm.com>
2020-04-13 17:05:45 -0500

    regulators: Add configure support to Rail class
    Implemented the configure() method in the Rail class.  This method
    applies any configuration changes that are defined for the rail.
    
    Also added a few missing #includes in the configuration_tests.cpp file.
    
    Signed-off-by: Shawn McCarney <shawnmm@us.ibm.com>
    Change-Id: I4164621f6d599bbf1417ebb9328a35bd2b2d656e

--- phosphor-regulators/test/rail_tests.cpp
+++ phosphor-regulators/test/rail_tests.cpp
@@ -14,10 +14,19 @@
  * limitations under the License.
  */
 #include "action.hpp"
+#include "chassis.hpp"
 #include "configuration.hpp"
+#include "device.hpp"
+#include "i2c_interface.hpp"
+#include "journal.hpp"
 #include "mock_action.hpp"
+#include "mock_journal.hpp"
+#include "mocked_i2c_interface.hpp"
+#include "presence_detection.hpp"
 #include "rail.hpp"
+#include "rule.hpp"
 #include "sensor_monitoring.hpp"
+#include "system.hpp"
 
 #include <memory>
 #include <optional>
@@ -27,6 +36,8 @@
 #include <gtest/gtest.h>
 
 using namespace phosphor::power::regulators;
+
+using ::testing::Return;
 
 TEST(RailTests, Constructor)
 {
@@ -64,6 +75,100 @@ TEST(RailTests, Constructor)
         EXPECT_EQ(rail.getConfiguration()->getActions().size(), 2);
         EXPECT_NE(rail.getSensorMonitoring(), nullptr);
         EXPECT_EQ(rail.getSensorMonitoring()->getActions().size(), 1);
+    }
+}
+
+TEST(RailTests, Configure)
+{
+    // Test where Configuration was not specified in constructor
+    {
+        // Create Rail
+        std::unique_ptr<Rail> rail = std::make_unique<Rail>("vdd0");
+        Rail* railPtr = rail.get();
+
+        // Create Device that contains Rail
+        std::unique_ptr<i2c::MockedI2CInterface> i2cInterface =
+            std::make_unique<i2c::MockedI2CInterface>();
+        std::unique_ptr<PresenceDetection> presenceDetection{};
+        std::unique_ptr<Configuration> deviceConfiguration{};
+        std::vector<std::unique_ptr<Rail>> rails{};
+        rails.emplace_back(std::move(rail));
+        std::unique_ptr<Device> device = std::make_unique<Device>(
+            "reg1", true, "/system/chassis/motherboard/reg1",
+            std::move(i2cInterface), std::move(presenceDetection),
+            std::move(deviceConfiguration), std::move(rails));
+        Device* devicePtr = device.get();
+
+        // Create Chassis that contains Device
+        std::vector<std::unique_ptr<Device>> devices{};
+        devices.emplace_back(std::move(device));
+        std::unique_ptr<Chassis> chassis =
+            std::make_unique<Chassis>(1, std::move(devices));
+        Chassis* chassisPtr = chassis.get();
+
+        // Create System that contains Chassis
+        std::vector<std::unique_ptr<Rule>> rules{};
+        std::vector<std::unique_ptr<Chassis>> chassisVec{};
+        chassisVec.emplace_back(std::move(chassis));
+        System system{std::move(rules), std::move(chassisVec)};
+
+        // Call configure().  Should do nothing.
+        journal::clear();
+        railPtr->configure(system, *chassisPtr, *devicePtr);
+        EXPECT_EQ(journal::getDebugMessages().size(), 0);
+        EXPECT_EQ(journal::getErrMessages().size(), 0);
+    }
+
+    // Test where Configuration was specified in constructor
+    {
+        // Create Configuration
+        std::optional<double> volts{1.3};
+        std::unique_ptr<MockAction> action = std::make_unique<MockAction>();
+        EXPECT_CALL(*action, execute).Times(1).WillOnce(Return(true));
+        std::vector<std::unique_ptr<Action>> actions{};
+        actions.emplace_back(std::move(action));
+        std::unique_ptr<Configuration> configuration =
+            std::make_unique<Configuration>(volts, std::move(actions));
+
+        // Create Rail
+        std::unique_ptr<Rail> rail =
+            std::make_unique<Rail>("vddr1", std::move(configuration));
+        Rail* railPtr = rail.get();
+
+        // Create Device that contains Rail
+        std::unique_ptr<i2c::MockedI2CInterface> i2cInterface =
+            std::make_unique<i2c::MockedI2CInterface>();
+        std::unique_ptr<PresenceDetection> presenceDetection{};
+        std::unique_ptr<Configuration> deviceConfiguration{};
+        std::vector<std::unique_ptr<Rail>> rails{};
+        rails.emplace_back(std::move(rail));
+        std::unique_ptr<Device> device = std::make_unique<Device>(
+            "reg1", true, "/system/chassis/motherboard/reg1",
+            std::move(i2cInterface), std::move(presenceDetection),
+            std::move(deviceConfiguration), std::move(rails));
+        Device* devicePtr = device.get();
+
+        // Create Chassis that contains Device
+        std::vector<std::unique_ptr<Device>> devices{};
+        devices.emplace_back(std::move(device));
+        std::unique_ptr<Chassis> chassis =
+            std::make_unique<Chassis>(1, std::move(devices));
+        Chassis* chassisPtr = chassis.get();
+
+        // Create System that contains Chassis
+        std::vector<std::unique_ptr<Rule>> rules{};
+        std::vector<std::unique_ptr<Chassis>> chassisVec{};
+        chassisVec.emplace_back(std::move(chassis));
+        System system{std::move(rules), std::move(chassisVec)};
+
+        // Call configure().  Should execute Configuration and log debug message
+        // to journal.
+        journal::clear();
+        railPtr->configure(system, *chassisPtr, *devicePtr);
+        std::vector<std::string> expectedDebugMessages{
+            "Configuring vddr1: volts=1.300000"};
+        EXPECT_EQ(journal::getDebugMessages(), expectedDebugMessages);
+        EXPECT_EQ(journal::getErrMessages().size(), 0);
     }
 }
 
