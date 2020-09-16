f6d77227274bd7660f4ff4661afa75de10b548f8
Brandon Wyman <bjwyman@gmail.com>
2020-09-16 18:06:06 -0500

    psu-ng: Create PELs for detected faults
    Add in a function for creating PELs using the Create D-Bus call.
    
    Update the analyze function to create PELs when faults are found.
    
    We would want to log a fault per power supply, so move the faultLogged
    concept down to the PowerSupply object itself.
    
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>
    Change-Id: I94d22b2b8a495abde87fb921c9177c6d25225ef7

--- phosphor-power-supply/psu_manager.hpp
+++ phosphor-power-supply/psu_manager.hpp
@@ -121,8 +121,6 @@ class PSUManager
         {
             psu->clearFaults();
         }
-
-        faultLogged = false;
     }
 
   private:
@@ -139,15 +137,20 @@ class PSUManager
         timer;
 
     /**
+     * Create a PEL
+     */
+    void createErrorPEL(const std::string& faultName,
+                        std::map<std::string, std::string> additionalData);
+
+    /**
      * Analyze the status of each of the power supplies.
+     *
+     * Log errors for faults, when and where appropriate.
      */
     void analyze();
 
     /** @brief True if the power is on. */
     bool powerOn = false;
-
-    /** @brief True if fault logged. Clear in clearFaults(). */
-    bool faultLogged = false;
 
     /** @brief Used as part of subscribing to power on state changes*/
     std::string powerService;
