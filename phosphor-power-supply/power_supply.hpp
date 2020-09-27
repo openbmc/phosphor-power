7e49527c2786c6b5d5e661d1bcb883ed065c0ec3
Brandon Wyman <bjwyman@gmail.com>
2020-09-26 19:57:46 -0500

    psu-ng: Add accessor to get the inventory path
    When creating errors, the inventory path may be needed for some of the
    call outs.
    
    Signed-off-by: Brandon Wyman <bjwyman@gmail.com>
    Change-Id: I545aae603478e43909620b087c57ccd4b69d3eef

--- phosphor-power-supply/power_supply.hpp
+++ phosphor-power-supply/power_supply.hpp
@@ -167,6 +167,11 @@ class PowerSupply
         return vinUVFault;
     }
 
+    const std::string& getInventoryPath() const
+    {
+        return inventoryPath;
+    }
+
   private:
     /** @brief systemd bus member */
     sdbusplus::bus::bus& bus;
