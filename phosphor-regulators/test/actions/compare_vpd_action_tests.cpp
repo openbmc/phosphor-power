b54cb0f373c76b74fbe429c23e6121ddd8a97ead
Bob King <Bob_King@wistron.com>
2020-12-02 13:34:06 +0800

    regulators: Implement CompareVPDAction class
    The CompareVPDAction::execute() method compares a VPD (Vital Product
    Data) keyword value to an expected value.
    It returns true if they match and false if they don't.
    
    Signed-off-by: Bob King <Bob_King@wistron.com>
    Change-Id: Ie98f16576937f25e27f3e465f9fbb03c1c917c96

--- phosphor-regulators/test/actions/compare_vpd_action_tests.cpp
+++ phosphor-regulators/test/actions/compare_vpd_action_tests.cpp
@@ -13,18 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "action.hpp"
 #include "action_environment.hpp"
+#include "action_error.hpp"
 #include "compare_vpd_action.hpp"
+#include "device.hpp"
+#include "i2c_interface.hpp"
 #include "id_map.hpp"
+#include "mock_action.hpp"
+#include "mock_services.hpp"
+#include "mock_vpd_service.hpp"
+#include "mocked_i2c_interface.hpp"
 
 #include <exception>
 #include <memory>
 #include <stdexcept>
 #include <utility>
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 using namespace phosphor::power::regulators;
+
+using ::testing::A;
+using ::testing::Return;
+using ::testing::Throw;
 
 TEST(CompareVPDActionTests, Constructor)
 {
@@ -39,7 +52,90 @@ TEST(CompareVPDActionTests, Constructor)
 
 TEST(CompareVPDActionTests, Execute)
 {
-    // TODO: Not implemented yet
+    // Test where works: actual keyword value is "2D35".
+    try
+    {
+        // Create mock I2CInterface
+        std::unique_ptr<i2c::MockedI2CInterface> i2cInterface =
+            std::make_unique<i2c::MockedI2CInterface>();
+
+        // Create Device, IDMap, mock services and ActionEnvironment
+        // Actual keyword value getValue() is "2D35"
+        Device device{
+            "reg1", true,
+            "/xyz/openbmc_project/inventory/system/chassis/motherboard/reg1",
+            std::move(i2cInterface)};
+        IDMap idMap{};
+        idMap.addDevice(device);
+        MockServices services{};
+        MockVPDService& vpdService = services.getMockVPDService();
+        EXPECT_CALL(vpdService,
+                    getValue(A<const std::string&>(), A<const std::string&>()))
+            .Times(2)
+            .WillRepeatedly(Return("2D35"));
+        ActionEnvironment env{idMap, "reg1", services};
+
+        // Test where works: expected value is 2D35, return value is true.
+        {
+            CompareVPDAction action{
+                "/xyz/openbmc_project/inventory/system/chassis/disk_backplane",
+                "CCIN", "2D35"};
+            EXPECT_EQ(action.execute(env), true);
+        }
+
+        // Test where works: expected value is VALUE, return value is false.
+        {
+            CompareVPDAction action{
+                "/xyz/openbmc_project/inventory/system/chassis/disk_backplane",
+                "CCIN", "VALUE"};
+            EXPECT_EQ(action.execute(env), false);
+        }
+    }
+    catch (...)
+    {
+        ADD_FAILURE() << "Should not have caught exception.";
+    }
+
+    // Test where fails. Reading value fails.
+    try
+    {
+        // Create mock I2CInterface
+        std::unique_ptr<i2c::MockedI2CInterface> i2cInterface =
+            std::make_unique<i2c::MockedI2CInterface>();
+
+        // Create Device, IDMap, mock services and ActionEnvironment
+        // vpdService cannot get the value
+        Device device{
+            "reg1", true,
+            "/xyz/openbmc_project/inventory/system/chassis/motherboard/reg1",
+            std::move(i2cInterface)};
+        IDMap idMap{};
+        idMap.addDevice(device);
+        MockServices services{};
+        MockVPDService& vpdService = services.getMockVPDService();
+        EXPECT_CALL(vpdService,
+                    getValue(A<const std::string&>(), A<const std::string&>()))
+            .Times(1)
+            .WillOnce(
+                Throw(std::runtime_error("vpdService cannot get the value.")));
+
+        ActionEnvironment env{idMap, "reg1", services};
+
+        CompareVPDAction action{
+            "/xyz/openbmc_project/inventory/system/chassis/disk_backplane",
+            "CCIN", "2D35"};
+        EXPECT_EQ(action.execute(env), false);
+    }
+    catch (const ActionError& e)
+    {
+        EXPECT_STREQ(e.what(), "ActionError: compare_vpd: { fru: "
+                               "/xyz/openbmc_project/inventory/system/chassis/"
+                               "disk_backplane, keyword: CCIN, value: 2D35 }");
+    }
+    catch (...)
+    {
+        ADD_FAILURE() << "Should not have caught exception.";
+    }
 }
 
 TEST(CompareVPDActionTests, GetFRU)
