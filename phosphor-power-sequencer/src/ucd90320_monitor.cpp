5e2aca6b2a397d9fe6739dffcf2568abe695babb
Jim Wright <jlwright@us.ibm.com>
2021-04-06 16:55:15 -0500

    pseq: Add base and UCD90320 device monitor classes
    Add a power sequencer device monitoring base class and a UCD90320
    subclass. Update power controller class to construct and pass device
    information to device class.  Update build file to support.
    
    Signed-off-by: Jim Wright <jlwright@us.ibm.com>
    Change-Id: Idf3ef53011b23f15841d93795f9f0995892b10b9

--- phosphor-power-sequencer/src/ucd90320_monitor.cpp
+++ phosphor-power-sequencer/src/ucd90320_monitor.cpp
@@ -14,7 +14,24 @@
  * limitations under the License.
  */
 
-int main()
+#include "ucd90320_monitor.hpp"
+
+#include <fmt/format.h>
+
+#include <string>
+
+namespace phosphor::power::sequencer
 {
-    return 0;
-}
+
+const std::string DRIVER_NAME = "ucd9000";
+
+UCD90320Monitor::UCD90320Monitor(sdbusplus::bus::bus& bus, std::uint8_t i2cBus,
+                                 std::uint16_t i2cAddress) :
+    PowerSequencerMonitor(),
+    bus{bus}, interface{fmt::format("/sys/bus/i2c/devices/{}-{:04x}", i2cBus,
+                                    i2cAddress)
+                            .c_str(),
+                        DRIVER_NAME, 0}
+{}
+
+} // namespace phosphor::power::sequencer
---
+++ phosphor-power-sequencer/src/ucd90320_monitor.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "pmbus.hpp"
+#include "power_sequencer_monitor.hpp"
+
+#include <sdbusplus/bus.hpp>
+
+namespace phosphor::power::sequencer
+{
+
+/**
+ * @class UCD90320Monitor
+ * This class implements fault analysis for the UCD90320
+ * power sequencer device.
+ */
+class UCD90320Monitor : public PowerSequencerMonitor
+{
+  public:
+    UCD90320Monitor() = delete;
+    UCD90320Monitor(const UCD90320Monitor&) = delete;
+    UCD90320Monitor& operator=(const UCD90320Monitor&) = delete;
+    UCD90320Monitor(UCD90320Monitor&&) = delete;
+    UCD90320Monitor& operator=(UCD90320Monitor&&) = delete;
+    ~UCD90320Monitor() = default;
+
+    /**
+     * Create a device object for UCD90320 monitoring.
+     * @param[in] bus D-Bus bus object
+     * @param[in] i2cBus The bus number of the power sequencer device
+     * @param[in] i2cAddress The 16-bit I2C address of the power sequencer
+     * device
+     */
+    UCD90320Monitor(sdbusplus::bus::bus& bus, std::uint8_t i2cBus,
+                    const std::uint16_t i2cAddress);
+
+  private:
+    /**
+     * The D-Bus bus object
+     */
+    sdbusplus::bus::bus& bus;
+
+    /**
+     * The read/write interface to this hardware
+     */
+    pmbus::PMBus interface;
+};
+
+} // namespace phosphor::power::sequencer
