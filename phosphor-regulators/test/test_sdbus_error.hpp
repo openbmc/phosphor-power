8c232456650aa241976f9955701eb8f9922aeb19
Shawn McCarney <shawnmm@us.ibm.com>
2021-05-05 18:44:57 -0500

    regulators: Complete PMBusReadSensorAction class
    Complete implementation of the PMBusReadSensorAction class.  Publish the
    sensor value on D-Bus using the Sensors service.
    
    Update gtests for this class to expect calls to the Sensors service.
    Fix some constructor parameter combinations that did not make sense,
    such as linear_16 format for non-voltage sensors.
    
    Also refactor the TestSDBusError exception class into a separate file.
    This allows it to be shared by multiple test cases.  This class is a
    concrete implementation of the abstract sdbusplus::exception_t class
    used for D-Bus errors.
    
    Signed-off-by: Shawn McCarney <shawnmm@us.ibm.com>
    Change-Id: I5a77616b2ae5b17224c296aeee88019512649589

---
+++ phosphor-regulators/test/test_sdbus_error.hpp
@@ -0,0 +1,54 @@
+/**
+ * Copyright © 2021 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <sdbusplus/exception.hpp>
+
+#include <string>
+
+namespace phosphor::power::regulators
+{
+
+/**
+ * Concrete subclass of sdbusplus::exception_t abstract base class.
+ */
+class TestSDBusError : public sdbusplus::exception_t
+{
+  public:
+    TestSDBusError(const std::string& error) : error{error}
+    {
+    }
+
+    const char* what() const noexcept override
+    {
+        return error.c_str();
+    }
+
+    const char* name() const noexcept override
+    {
+        return "";
+    }
+
+    const char* description() const noexcept override
+    {
+        return "";
+    }
+
+  private:
+    const std::string error{};
+};
+
+} // namespace phosphor::power::regulators
