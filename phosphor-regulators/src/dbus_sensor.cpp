a6461342211da559c0c610fb65f780932f9db881
Patrick Williams <patrick@stwcx.xyz>
2022-04-05 21:22:58 -0500

    sdbusplus: object: don't use 'bool' argument constructor
    `sdbusplus::server::object_t` has long had an enum-based parameter for
    signal action, but maintained a backwards compatible boolean mapping.
    It is time to remove this boolean to make it more observable which
    actions are being used in applications.  Map all `true` occurrences to
    `action::defer_emit`.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ied6d1b6116b07a8f73b398a098298f4990b24818

--- phosphor-regulators/src/dbus_sensor.cpp
+++ phosphor-regulators/src/dbus_sensor.cpp
@@ -85,9 +85,8 @@ DBusSensor::DBusSensor(sdbusplus::bus::bus& bus, const std::string& name,
 
     // Create the sdbusplus object that implements the D-Bus sensor interfaces.
     // Skip emitting D-Bus signals until the object has been fully created.
-    bool skipSignal{true};
-    dbusObject =
-        std::make_unique<DBusSensorObject>(bus, objectPath.c_str(), skipSignal);
+    dbusObject = std::make_unique<DBusSensorObject>(
+        bus, objectPath.c_str(), DBusSensorObject::action::defer_emit);
 
     // Set properties of the Value interface
     dbusObject->value(value, skipSignal);
