944ff61c1e8c66dcbeb6b621136f868ee6dcd1fe
Patrick Williams <patrick@stwcx.xyz>
2022-12-08 06:23:33 -0600

    beautysh: re-format
    beautysh is enabled in the openbmc-build-scripts on Bash/Zsh/POSIX-sh
    files to have a consistent formatting.  Re-run the formatter on the
    whole repository.
    
    Change-Id: If2be5279a84f72f4b8d58a978305b7c650d4f5be
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- phosphor-regulators/test/run-ci.sh
+++ phosphor-regulators/test/run-ci.sh
@@ -3,7 +3,7 @@ rc=0
 for file in phosphor-regulators/config_files/*.json
 do
     phosphor-regulators/tools/validate-regulators-config.py \
-    -s phosphor-regulators/schema/config_schema.json \
-    -c "$file" || rc=1
+        -s phosphor-regulators/schema/config_schema.json \
+        -c "$file" || rc=1
 done
 exit $rc
