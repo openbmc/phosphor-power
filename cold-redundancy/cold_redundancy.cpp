e7abddf50c17afa367cce243414a003fda89e2fb
George Liu <liuxiwei@ieisystem.com>
2025-12-17 16:26:17 +0800

    Remove redundant is_method_error() checks
    The handlers registered through sdbusplus::bus::match_t only receive
    D-Bus signals. Signal messages are never sent as method-error
    replies, and therefore message.is_method_error() can never be true in
    these callbacks.
    
    This change removes all unnecessary is_method_error() checks from
    signal handlers to simplify the code and avoid confusion.
    
    Change-Id: I6f3289e7f05b39ef59eebc5b9ef4e07676c62a5c
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- cold-redundancy/cold_redundancy.cpp
+++ cold-redundancy/cold_redundancy.cpp
@@ -49,11 +49,6 @@ ColdRedundancy::ColdRedundancy(
     post(io, [this, &systemBus]() { createPSU(systemBus); });
     std::function<void(sdbusplus::message_t&)> eventHandler =
         [this, &systemBus](sdbusplus::message_t& message) {
-            if (message.is_method_error())
-            {
-                std::cerr << "callback method error\n";
-                return;
-            }
             filterTimer.expires_after(std::chrono::seconds(1));
             filterTimer.async_wait(
                 [this, &systemBus](const boost::system::error_code& ec) {
