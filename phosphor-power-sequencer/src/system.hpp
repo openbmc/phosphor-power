4c832f9bc2dba91251d31fe234bece1783a1d04d
Shawn McCarney <shawnmm@us.ibm.com>
2026-01-02 14:37:17 -0600

    pseq: Add power state/power good to System class
    Add the following to the power sequencer System class:
    * Power state and power good data members
    * Methods to get power state and power good values
    * Method to set the power state to power the system on/off
    * Method to monitor the system and update the power good value
    
    Tested:
    * Ran automated tests
    * Tested new methods in simulation:
      * Verified getPowerState() returned correct value when system on/off
      * Verified setPowerState() set the power control GPIO correctly
      * Verified getPowerGood() returned correct value when system on/off
      * Verified monitor() obtained the current power good value when system
        on/off
    
    Change-Id: Ie7f01bccac330240a3d8173805d77badb5f5b3c8
    Signed-off-by: Shawn McCarney <shawnmm@us.ibm.com>

--- phosphor-power-sequencer/src/system.hpp
+++ phosphor-power-sequencer/src/system.hpp
@@ -16,8 +16,15 @@
 #pragma once
 
 #include "chassis.hpp"
+#include "power_interface.hpp"
+#include "services.hpp"
+
+#include <stddef.h> // for size_t
 
 #include <memory>
+#include <optional>
+#include <set>
+#include <stdexcept>
 #include <utility>
 #include <vector>
 
@@ -60,11 +67,204 @@ class System
         return chassis;
     }
 
+    /**
+     * Initializes system monitoring.
+     *
+     * This method must be called before any methods that return or check the
+     * system status.
+     *
+     * Normally this method is only called once. However, it can be called
+     * multiple times if required, such as for automated testing.
+     *
+     * @param services System services like hardware presence and the journal
+     */
+    void initializeMonitoring(Services& services);
+
+    /**
+     * Returns the last requested system power state.
+     *
+     * The initial power state is obtained by the monitor() method. That method
+     * must be called before calling getPowerState().
+     *
+     * Throws an exception if the power state could not be obtained.
+     *
+     * @return last requested power state
+     */
+    PowerState getPowerState()
+    {
+        if (!powerState)
+        {
+            throw std::runtime_error{
+                "System power state could not be obtained"};
+        }
+        return *powerState;
+    }
+
+    /**
+     * Sets the requested system power state.
+     *
+     * Powers the system on or off based on the specified state.
+     *
+     * Throws an exception if one of the following occurs:
+     * - System monitoring has not been initialized
+     * - System cannot be set to the specified power state
+     * - No chassis can be set to the specified power state
+     *
+     * @param newPowerState New system power state
+     * @param services System services like hardware presence and the journal
+     */
+    void setPowerState(PowerState newPowerState, Services& services);
+
+    /**
+     * Returns the chassis numbers selected for the current power on/off
+     * attempt.
+     *
+     * @return chassis numbers in current power on/off attempt
+     */
+    const std::set<size_t>& getSelectedChassis()
+    {
+        return selectedChassis;
+    }
+
+    /**
+     * Returns the system power good value.
+     *
+     * The power good value is set by the monitor() method. That method must be
+     * called before calling getPowerGood().
+     *
+     * Throws an exception if the power good value could not be obtained.
+     *
+     * @return system power good
+     */
+    PowerGood getPowerGood()
+    {
+        if (!powerGood)
+        {
+            throw std::runtime_error{"System power good could not be obtained"};
+        }
+        return *powerGood;
+    }
+
+    /**
+     * Monitors the status of the system.
+     *
+     * Sets the system power good value by obtaining the power good value from
+     * each chassis selected for the current power on/off attempt.
+     *
+     * This method must be called periodically (such as once per second) to
+     * ensure the system power state and power good values are correct.
+     *
+     * Throws an exception if system monitoring has not been initialized.
+     *
+     * @param services System services like hardware presence and the journal
+     */
+    void monitor(Services& services);
+
   private:
+    /**
+     * Verifies that system monitoring has been initialized.
+     *
+     * Throws an exception if monitoring has not been initialized.
+     */
+    void verifyMonitoringInitialized()
+    {
+        if (!isMonitoringInitialized)
+        {
+            throw std::runtime_error{
+                "System monitoring has not been initialized"};
+        }
+    }
+
+    /**
+     * Verifies that the system can be set to the specified power state.
+     *
+     * Throws an exception if the new power state is not allowed.
+     *
+     * @param newPowerState New system power state
+     */
+    void verifyCanSetPowerState(PowerState newPowerState)
+    {
+        if (powerState && (powerState == newPowerState))
+        {
+            throw std::runtime_error{std::format(
+                "Unable to set system to state {}: Already at requested state",
+                PowerInterface::toString(newPowerState))};
+        }
+    }
+
+    /**
+     * Returns the set of chassis numbers that can be set to the specified new
+     * power state.
+     *
+     * Throws an exception if system monitoring has not been initialized.
+     *
+     * @param newPowerState New system power state
+     * @param services System services like hardware presence and the journal
+     * @return set of chassis numbers that can be set to new power state
+     */
+    std::set<size_t> getChassisForNewPowerState(PowerState newPowerState,
+                                                Services& services);
+
+    /**
+     * Defines the initial set of chassis selected for power on/off if needed.
+     *
+     * This is necessary when the application first starts.
+     *
+     * We do not know which chassis were selected for power on/off prior to the
+     * application starting.
+     *
+     * Define the selected chassis set based on which chassis are currently
+     * powered on and off.
+     *
+     * The selected chassis will be set explicitly next time the system is
+     * powered on or off by setPowerState().
+     */
+    void setInitialSelectedChassisIfNeeded();
+
+    /**
+     * Sets the system power good value based on the chassis power good values.
+     */
+    void setPowerGood();
+
+    /**
+     * Sets the initial power state value if it currently has no value.
+     *
+     * This is necessary when the application first starts.
+     *
+     * The initial power state value is based on the current power good value.
+     * We assume that the last requested power state matches the power good
+     * value. For example, if the system power good is on, then we assume the
+     * last requested system power state was on.
+     *
+     * The power state value will be set explicitly next time the system is
+     * powered on or off by setPowerState().
+     */
+    void setInitialPowerStateIfNeeded();
+
     /**
      * Chassis in the system.
      */
     std::vector<std::unique_ptr<Chassis>> chassis{};
+
+    /**
+     * Indicates whether system monitoring has been initialized.
+     */
+    bool isMonitoringInitialized{false};
+
+    /**
+     * Last requested system power state.
+     */
+    std::optional<PowerState> powerState{};
+
+    /**
+     * System power good.
+     */
+    std::optional<PowerGood> powerGood{};
+
+    /**
+     * Chassis numbers that were selected for the current power on/off attempt.
+     */
+    std::set<size_t> selectedChassis;
 };
 
 } // namespace phosphor::power::sequencer
