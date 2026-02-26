67acbe64d4b3d0db22b414ce3ae19fdca1d65b0d
Sheldon Bailey <baileysh@us.ibm.com>
2026-02-26 11:38:23 -0600

    Create config file parser that returns chassis and gpio objects
    Create parsing use case that reads in config file and creates the
    proper chassis and gpio objects to return back to caller.
    
    Tested:
    - Verified test cases of objects Gpio, Chassis, objects
    - Verified test case of parser reading config file and returning
        objects correctly.
    - Verified simulation that parser reads from BMC config file and
        creates objects returned to caller.
    
    Change-Id: I87a405142e63d0fc2ed36da4d6b5e5bd52b2b74f
    Signed-off-by: Sheldon Bailey <baileysh@us.ibm.com>

---
+++ phosphor-chassis-power/src/config_file_parser.cpp
@@ -0,0 +1,190 @@
+/**
+ * Copyright © 2026 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "config_file_parser.hpp"
+
+#include "config_file_parser_error.hpp"
+#include "json_parser_utils.hpp"
+
+#include <cstdint>
+#include <exception>
+#include <fstream>
+#include <optional>
+#include <stdexcept>
+#include <utility>
+
+using json = nlohmann::json;
+using ConfigFileParserError = phosphor::power::util::ConfigFileParserError;
+using namespace phosphor::power::json_parser_utils;
+
+namespace phosphor::power::chassis::config_file_parser
+{
+
+std::vector<std::unique_ptr<Chassis>> parse(
+    const std::filesystem::path& pathName)
+{
+    try
+    {
+        // Use standard JSON parser to create tree of JSON elements
+        std::ifstream file{pathName};
+        json rootElement = json::parse(file);
+
+        // Parse tree of JSON elements and return corresponding C++ objects
+        return internal::parseRoot(rootElement);
+    }
+    catch (const std::exception& e)
+    {
+        throw ConfigFileParserError{pathName, e.what()};
+    }
+}
+
+namespace internal
+{
+
+std::unique_ptr<Chassis> parseChassis(const json& element)
+{
+    verifyIsObject(element);
+    unsigned int propertyCount{0};
+
+    // Required number property
+    const json& numberElement = getRequiredProperty(element, "ChassisNumber");
+    unsigned int number = parseUnsignedInteger(numberElement);
+    if (number < 1)
+    {
+        throw std::invalid_argument{"Invalid chassis number: Must be > 0"};
+    }
+    ++propertyCount;
+
+    // Optional presence_path property
+    std::optional<std::string> inventoryPath{};
+    auto inventoryPathElement = element.find("PresencePath");
+    if (inventoryPathElement != element.end())
+    {
+        inventoryPath = parseInventoryPath(*inventoryPathElement);
+        ++propertyCount;
+    }
+
+    // Optional GPIO
+    std::vector<std::unique_ptr<Gpio>> gpios{};
+    // Optional PresenceGpio
+    auto PresenceGpioIt = element.find("PresenceGpio");
+    if (PresenceGpioIt != element.end())
+    {
+        gpios.emplace_back(parseGpio(*PresenceGpioIt));
+        ++propertyCount;
+    }
+
+    // Optional FaultUnlatchedGpio
+    auto FaultUnlatchedGpioIt = element.find("FaultUnlatchedGpio");
+    if (FaultUnlatchedGpioIt != element.end())
+    {
+        gpios.emplace_back(parseGpio(*FaultUnlatchedGpioIt));
+        ++propertyCount;
+    }
+
+    // Optional FaultLatchedGpio
+    auto FaultLatchedGpioIt = element.find("FaultLatchedGpio");
+    if (FaultLatchedGpioIt != element.end())
+    {
+        gpios.emplace_back(parseGpio(*FaultLatchedGpioIt));
+        ++propertyCount;
+    }
+
+    // Optional FaultLatchResetGpio
+    auto FaultLatchResetGpioIt = element.find("FaultLatchResetGpio");
+    if (FaultLatchResetGpioIt != element.end())
+    {
+        gpios.emplace_back(parseGpio(*FaultLatchResetGpioIt));
+        ++propertyCount;
+    }
+
+    // Optional EnableSystemResetGpio
+    auto EnableSystemResetGpioIt = element.find("EnableSystemResetGpio");
+    if (EnableSystemResetGpioIt != element.end())
+    {
+        gpios.emplace_back(parseGpio(*EnableSystemResetGpioIt));
+        ++propertyCount;
+    }
+
+    // Verify no invalid properties exist
+    verifyPropertyCount(element, propertyCount);
+
+    return std::make_unique<Chassis>(number, std::move(inventoryPath),
+                                     std::move(gpios));
+}
+
+std::vector<std::unique_ptr<Chassis>> parseChassisArray(const json& element)
+{
+    verifyIsArray(element);
+    std::vector<std::unique_ptr<Chassis>> chassis;
+    for (auto& chassisElement : element)
+    {
+        chassis.emplace_back(parseChassis(chassisElement));
+    }
+    return chassis;
+}
+
+std::unique_ptr<Gpio> parseGpio(const json& element)
+{
+    verifyIsObject(element);
+    unsigned int propertyCount{0};
+
+    // Required Name property
+    const json& nameElement = getRequiredProperty(element, "Name");
+    std::string name = parseString(nameElement);
+    ++propertyCount;
+
+    // Required Direction property
+    const json& directionElement = getRequiredProperty(element, "Direction");
+    std::string directionStr = parseString(directionElement);
+    gpioDirection direction = parseDirection(directionStr);
+    ++propertyCount;
+
+    // Required Polarity property
+    const json& polarityElement = getRequiredProperty(element, "Polarity");
+    std::string polarityStr = parseString(polarityElement);
+    gpioPolarity polarity = parsePolarity(polarityStr);
+    ++propertyCount;
+
+    // Verify no invalid properties exist
+    verifyPropertyCount(element, propertyCount);
+
+    return std::make_unique<Gpio>(name, direction, polarity);
+}
+
+std::string parseInventoryPath(const json& element)
+{
+    std::string inventoryPath = parseString(element);
+    std::string absPath = "/xyz/openbmc_project/inventory";
+    if (inventoryPath.front() != '/')
+    {
+        absPath += '/';
+    }
+    absPath += inventoryPath;
+    return absPath;
+}
+
+std::vector<std::unique_ptr<Chassis>> parseRoot(const json& element)
+{
+    verifyIsArray(element);
+
+    // Parse the array of chassis objects
+    return parseChassisArray(element);
+}
+
+} // namespace internal
+
+} // namespace phosphor::power::chassis::config_file_parser
