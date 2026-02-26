48fbb9c94e60706414f44762282d38658024df4f
Sheldon Bailey <baileysh@us.ibm.com>
2026-02-26 11:38:23 -0600

    Create config file parser that returns chassis and gpio objects
    Create parsing use case that reads in config file and creates the
    proper chassis and gpio objects to return back to caller.
    
    Tested:
    - Verified test cases of objects Gpio, Chassis, objects
    - Verified test case of parser reading config file and returning
        objects correctly.
    - Verified simulation that parser reads from BMC config file and
        creates objects returned to caller.
    
    Change-Id: I87a405142e63d0fc2ed36da4d6b5e5bd52b2b74f
    Signed-off-by: Sheldon Bailey <baileysh@us.ibm.com>

---
+++ phosphor-chassis-power/src/gpio.hpp
@@ -0,0 +1,146 @@
+/**
+ * Copyright © 2026 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace phosphor::power::chassis
+{
+
+// Forward declarations to avoid circular dependencies
+class Chassis;
+class System;
+
+/**
+ * @enum gpioDirection
+ *
+ * GPIO pin direction
+ */
+enum class gpioDirection
+{
+    Input,
+    Output
+};
+
+/**
+ * @enum gpioPolarity
+ *
+ * GPIO pin polarity
+ */
+enum class gpioPolarity
+{
+    Low,
+    High
+};
+
+/**
+ * @class Gpio
+ *
+ * A hardware device, such as a voltage regulator or I/O expander.
+ */
+class Gpio
+{
+  public:
+    // Specify which compiler-generated methods we want
+    Gpio() = delete;
+    Gpio(const Gpio&) = delete;
+    Gpio(Gpio&&) = delete;
+    Gpio& operator=(const Gpio&) = delete;
+    Gpio& operator=(Gpio&&) = delete;
+    ~Gpio() = default;
+
+    /**
+     * Constructor.
+     *
+     * @param name unique device name
+     * @param gpioDirection direction Output,Input
+     * @param gpioPolarity polarity Low,High
+     */
+    explicit Gpio(const std::string& name, gpioDirection direction,
+                  gpioPolarity polarity) :
+        name{std::move(name)}, direction{std::move(direction)},
+        polarity{std::move(polarity)}
+    {}
+
+    /**
+     * Returns the unique name of this device.
+     *
+     * @return device name
+     */
+    const std::string& getName() const
+    {
+        return name;
+    }
+
+    /**
+     * Returns the direction of this device.
+     *
+     * @return device direction
+     */
+    gpioDirection getDirection() const
+    {
+        return direction;
+    }
+
+    /**
+     * Returns the polarity of this device.
+     *
+     * @return device polarity
+     */
+    gpioPolarity getPolarity() const
+    {
+        return polarity;
+    }
+
+  private:
+    /**
+     * Unique name of this device.
+     */
+    const std::string name{};
+
+    /**
+     * Direction of GPIO pin.
+     */
+    const gpioDirection direction{};
+
+    /**
+     * Polarity of GPIO pin.
+     */
+    const gpioPolarity polarity{};
+};
+
+/**
+ * Parses a string to a gpioDirection enum value.
+ *
+ * @param directionStr Direction string ("Input" or "Output")
+ * @return gpioDirection enum value
+ * @throws std::invalid_argument if string is invalid
+ */
+gpioDirection parseDirection(const std::string& directionStr);
+
+/**
+ * Parses a string to a gpioPolarity enum value.
+ *
+ * @param polarityStr Polarity string ("Low" or "High")
+ * @return gpioPolarity enum value
+ * @throws std::invalid_argument if string is invalid
+ */
+gpioPolarity parsePolarity(const std::string& polarityStr);
+
+} // namespace phosphor::power::chassis
