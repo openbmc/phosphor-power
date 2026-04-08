d106b519a65c5087e361d3ecf629d6ea3a743240
Brendan Singer <brendansinger2@gmail.com>
2026-04-08 16:27:20 +0000

    PCP Initialize chassis power GPIO Definitions
    Creates a gpio interface to implement gpiod functions.
    Implements necessary functions for claiming and releasing
    GPIO lines, reading and writing to gpios, and clearing the
    gpio error history.
    
    Tested:
    - Verified GPIOs could be claimed and released in simulation
    - Verified GPIOs could be read in simulation
    - Verified gpio clear error works when manually setting the
    associated property on the dbus
    
    Change-Id: I9e0d67996f56c5237649c1fe9850f157d0659b65
    Signed-off-by: Brendan Singer <brendansinger2@gmail.com>

--- phosphor-chassis-power/src/manager.hpp
+++ phosphor-chassis-power/src/manager.hpp
@@ -16,11 +16,15 @@
 #pragma once
 
 #include "compatible_system_types_finder.hpp"
+#include "services.hpp"
 #include "system.hpp"
 
 #include <sdbusplus/bus.hpp>
+#include <sdbusplus/bus/match.hpp>
+#include <sdbusplus/server/object.hpp>
 #include <sdeventplus/event.hpp>
 #include <sdeventplus/utility/timer.hpp>
+#include <xyz/openbmc_project/State/Chassis/server.hpp>
 
 #include <filesystem>
 #include <memory>
@@ -29,6 +33,7 @@
 
 namespace phosphor::power::chassis
 {
+
 /*
  * @class Manager
  *
@@ -96,6 +101,13 @@ class Manager
         system.reset();
     }
 
+    /**
+     * @brief Implementation for the clearErrorHistory D-Bus method
+     *
+     * Clears hardware error history for all chassis.
+     */
+    void clearErrorHistory();
+
   private:
     /**
      * Callback to begin failure process after not finding a Compatible system.
@@ -129,9 +141,16 @@ class Manager
     void loadConfigFile();
 
     /**
+     * Callback function to handle chassis power state changes.
+     *
+     * @param msg D-Bus message containing the property change
+     */
+    void chassisPowerStateChanged(sdbusplus::message_t& msg);
+
+    /**
      * The D-Bus bus
      */
-    sdbusplus::bus_t& bus [[maybe_unused]];
+    sdbusplus::bus_t& bus;
 
     /**
      * Event to loop on
@@ -164,6 +183,16 @@ class Manager
      * Contains nullptr if the configuration file has not been loaded.
      */
     std::unique_ptr<System> system;
+
+    /**
+     * Platform services provider.
+     */
+    BMCServices services;
+
+    /**
+     * D-Bus match object for monitoring chassis 0 power state changes.
+     */
+    std::unique_ptr<sdbusplus::bus::match_t> chassisPowerStateMatch;
 };
 
 } // namespace phosphor::power::chassis
